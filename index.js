a2d416612a105651cb0af7ec233bc869f77a1b17
ferdous <papiaferdous43@gmail.com>
2023-11-07 18:23:24 +0600

    complete crud delete operation

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/order/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    })
+
     app.get('/allfood', async(req, res) =>{
       const page = parseInt(req.query.page);
       const size = parseInt(req.query.size);
